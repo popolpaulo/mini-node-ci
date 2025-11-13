13825d9074e576670c45ed16afb8b554ee2a8662
Paul <paul.jlb.morle@gmail.com>
2025-11-13 16:11:15 +0100

    push

--- src/calc.js
+++
@@ -1,9 +0,0 @@
-export const calc = {
-  add: (a, b) => a + b,
-  sub: (a, b) => a - b,
-  mul: (a, b) => a * b,
-  div: (a, b) => {
-    if (b === 0) throw new RangeError('Division by zero');
-    return a / b;
-  },
-};
